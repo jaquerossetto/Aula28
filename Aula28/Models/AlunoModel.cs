d8ad9b7321317da10020508240b71ff987d46b64
Jaqueline <jaquerossetto@hotmail.com>
2022-07-13 16:21:18 -0300

    Adicionar arquivos de projeto.

---
+++ Aula28/Models/AlunoModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Aula28.Models
+{
+    public class AlunoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Status { get; set; }
+
+        public AlunoModel(int id, string nome, string sobrenome, string status)
+        {
+            Id = id;
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Status = status;
+        }
+    }
+}
